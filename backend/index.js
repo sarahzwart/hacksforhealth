6947ce5202fa65c7ce436abc17061916c30bedb4
Ben Shain <besa26@lehigh.edu>
2024-03-02 12:49:46 -0500

    DFHD

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express")
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -21,7 +23,7 @@ app.post('/signup/therapist', async (req, res) => {
   const { username, password } = req.body;
   
 
-  if (!username || !password || !groupId) {
+  if (!username || !password ) {
     return res.status(400).json({ message: 'Username, password, and group id are required' });
   }
 
@@ -86,10 +88,72 @@ app.get('/', (req, res) => {
 
 const jwtSecret = process.env.JWT_SECRET_KEY  
 
-
-
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+app.get('/health', async (req, res) => {
+  try {
+  const { username, hashedPassword } = req.body;
+  const result = await pool.query('SELECT (username, password) FROM ($1, $2)', [username, hashedPassword]);
+
+  const userId = result.rows[0].id;
+  return userId;
+  }
+  catch(err) {
+
+    console.error(err);
+    return res.status(500).json({ message: 'Invalid Username or Password' });
+  }
+
+
+});
+
+app.get('/Happy', async (req, res) => {
+  try {
+    const {PID} = req.body;
+  const result = await pool.query('SELECT (HAKey) FROM ($1)', [PID]);
+
+  return result;
+  }
+  catch(err) {
+
+    console.error(err);
+    return res.status(500).json({ message: 'Invalid Username or Password' });
+  }
+
+
+});
+
+app.post('/signin/patient', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  try {
+    const user = await pool.query('SELECT * FROM therapist WHERE username = $1', [username]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.rows[0].password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    const userId = user.rows[0].id;
+    const userName= user.rows[0].username;
+    
+    //console.log(userName)
+    const token = jwt.sign({ username: userName }, jwtSecret, { expiresIn: '2h' });
+
+    return res.status(200).json({ message: 'User authenticated successfully', userId, token, userName,bio,groupId });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 });

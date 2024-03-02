9d0f39acf2d96ece176deb090c9ad11ed1fd30c5
anhvu21 <Vuphamducanh2000@gmail.com>
2024-03-02 12:10:52 -0500

    e

--- backend/db.js
+++ backend/db.js
@@ -23,36 +23,4 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   port: process.env.PG_PORT,
-// };
-
-// const isProduction = process.env.NODE_ENV === "production";const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;const pool = new Pool({
-//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//     ssl: {
-//         rejectUnauthorized: false,
-//     },
-// });
-/*const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-}); /*
-/*
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
-const proConfig = process.env.DATABASE_URL; //heroku addons
-
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
-//console.log(pool)
-*/
 module.exports = pool;
